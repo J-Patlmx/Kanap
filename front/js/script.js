4cd50a03c008b6f433b0ed4176beb2b2e4880d95
J-Patlmx <lamoureux.jp61@gmail.com>
2022-12-02 11:35:47 +0100

    add product.js Wip script.js index modfied

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,20 @@
-console.log('hello world');
+let items = document.querySelector('#items')
 
+fetch("http://localhost:3000/api/products")
+    .then(res => res.json())
+    .then(data => {
+        let display = ''
+        for (let article of data) {
+            display += `
+                <a href="./product.html?id=${article._id}">
+                <article>
+                <img src="${article.imageUrl}" alt="${article.altTxt}">
+                <h3 class="productName">${article.name}</h3>
+                <p class="productDescription">${article.description}</p>
+                </article>
+                </a>
+                `
+        }
+        items.innerHTML = display
 
-
+    })
