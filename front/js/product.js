22d46ec49867beda8da33e8fe62853a2e069fbec
J-Patlmx <lamoureux.jp61@gmail.com>
2022-12-22 15:52:04 +0100

    add product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,89 +1,87 @@
 
-// const articleData = JSON.parse(localStorage.getItem("produit"))
-let produit = "";
+let product = "";
 //associer la page aux données de l'api
 let item = 0
 let str = window.location.href
 let url = new URL(str)
-const id = url.searchParams.get("id"); //<-
+const idProduct = url.searchParams.get("id"); //<- va chercher dans mon url la valeur du parametre id
 
+const colorsOption = document.getElementById('colors');
 
-
-fetch("http://localhost:3000/api/products" + id)// recuperation de l'api et stockage dans sessionStorage
+fetch("http://localhost:3000/api/products/" + idProduct)// recuperation de l'api et stockage dans localStorage
     .then((res) => res.json())//des promesses
     .then((data) => {//toujours des promesses
-        produit = data;
 
+        product = data;
+        // je recherche ce que je veux remplacer puis j'insere mon elemement de remplacement
+        //apres le debut de ma balise
         document.querySelector('.item__img')
-            .insertAdjacentHTML('afterbegin', `<img src="${produit.imageUrl}" alt="${produit.altTxt}"></img>`)
+            .insertAdjacentHTML('afterbegin', `<img src="${product.imageUrl}" alt="${product.altTxt}"></img>`)
         document.getElementById('title')
-            .insertAdjacentHTML('afterbegin', `${produit.name}`)
+            .insertAdjacentHTML('afterbegin', `${product.name}`)
         document.getElementById('price')
-            .insertAdjacentHTML('afterbegin', `${produit.price}`)
+            .insertAdjacentHTML('afterbegin', `${product.price}`)
         document.getElementById('description')
-            .insertAdjacentHTML('afterbegin', `${produit.description}`)
+            .insertAdjacentHTML('afterbegin', `${product.description}`)
+
+        // boucle permettant dafficher le choix des couleurs
+        // let colorsChoice = 0;
+        for (let _color of product.colors) {
+            document.getElementById('colors')
+                .insertAdjacentHTML('beforeend', `<option value=${_color}>${_color}</option>`);
+            // colorsChoice++
+        }
+    }
+        // }
+    )
+    .catch(function (_err) {
+        alert(' system failed');
     })
-// .catch(function (_err) {
-//     alert(' system failed');
-// })
-
-// boucle permettant dafficher le choix des couleurs
-let colorsChoice = 0;
-for (let _color of produit[_id].colors) {
-    document.getElementById('colors')
-        .insertAdjacentHTML('afterbegin', `<option value=${articleData[item].colors[colorsChoice]}>${articleData[item].colors[colorsChoice]}</option>`);
-    colorsChoice++
-}
 
 
-
-// fonction d'envoie au local storage
-const addLocalStorage = function (panier) {
-    localStorage.setItem('panier', JSON.stringify(panier));
-};
 //fonction d'ajout au panier
 const addToCart = () => {
     let quantity = parseInt(document.getElementById('quantity').value);
     let color = colorsOption.options[colorsOption.selectedIndex].value;
+    if (color === "") {
+        alert('Veuillez selectioner une couleur svp !')
+    } else if (quantity < 1 || quantity > 100) {
+        alert('Veuillez selectioner une quantité comprise entre 1 et 100 svp !')
 
-    let newPurchase = {
-        "_id": produit,
-        "quantity": quantity,
-        "color": color,
-    }
-}
-
-//recupere mon panier memorise
-let panier = localStorage.getItem('panier') ? JSON.parse(localStorage.getItem('panier')) : [];
-
-// boucle for qui parcours les ligne de mon panier
-let existingProduct = false;
-
-for (let i = 0; i < panier.length; i++) {
-    //si le produit exitse deja 
-    if (panier[i]._id === newPurchase._id && panier[i].colors === newPurchase.colors) {
-        existingProduct = i;
-    };
-    if (existingProduct !== false) {
-        panier[existingProduct].quantity = parseInt(panier[existingProduct].quantity) + newPurchase.quantity;
     } else {
-        panier.push(newPurchase);
+
+
+        let newPurchase = {
+            "_id": idProduct,
+            "quantity": quantity,
+            "color": color,
+        }
+
+        //recupere mon panier memorise
+        let panier = localStorage.getItem('panier') ? JSON.parse(localStorage.getItem('panier')) : [];
+
+        // boucle for qui parcours les ligne de mon panier
+        let existingProduct = false;
+
+        for (let i = 0; i < panier.length; i++) {
+            //si le produit exitse deja 
+            if (panier[i]._id === newPurchase._id && panier[i].color === newPurchase.color) {
+                existingProduct = i;
+            };
+        }
+        if (existingProduct !== false) {
+            panier[existingProduct].quantity = parseInt(panier[existingProduct].quantity) + newPurchase.quantity;
+        } else {
+            panier.push(newPurchase);
+        }
+        // addLocalStorage(panier);
+        localStorage.setItem('panier', JSON.stringify(panier));
+        location.replace("cart.html");
     }
 }
-addLocalStorage(panier);
-location.replace("front/html/cart.html");
 
+document.getElementById("addToCart").addEventListener("click", addToCart);
 
-// document.getElementById("addToCart").addEventListener("click", addToCart);
-
-// function newPurchase() {
-//     let panier = localStorage.getItem("panier");
-//     if (panier == null) {
-//         return [];
-//     } else {
-//         return JSON.parse(panier);
-//     }
-// }
 
 //-------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------
@@ -125,11 +123,4 @@ location.replace("front/html/cart.html");
 // }
 
 
-
-
 // }
-
-// function addToCart() {
-//     console.log(newPurchase);
-// }
-// condition ternaire
