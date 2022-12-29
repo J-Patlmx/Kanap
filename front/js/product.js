8f7f0e7385e1f9c0803dd1bf9ec00cc32a4eac6c
J-Patlmx <lamoureux.jp61@gmail.com>
2022-12-29 20:59:52 +0100

    add product

--- front/js/product.js
+++ front/js/product.js
@@ -25,14 +25,12 @@ fetch("http://localhost:3000/api/products/" + idProduct)// recuperation de l'api
             .insertAdjacentHTML('afterbegin', `${product.description}`)
 
         // boucle permettant dafficher le choix des couleurs
-        // let colorsChoice = 0;
         for (let _color of product.colors) {
             document.getElementById('colors')
                 .insertAdjacentHTML('beforeend', `<option value=${_color}>${_color}</option>`);
-            // colorsChoice++
+
         }
     }
-        // }
     )
     .catch(function (_err) {
         alert(' system failed');
@@ -88,39 +86,3 @@ document.getElementById("addToCart").addEventListener("click", addToCart);
 //-------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------
-
-
-
-// recherche du tableau en fonction de l'id de l'article via une boucle RETIRER CETTE BOUCLE
-// while (id != (articleData[item]._id)) {
-//     item++;
-//     var pageID = item;//la porte de la variable est uniquement dispo dans ma boucle
-// };
-
-// ajout des elements html
-// document.querySelector('.item__img')
-//     .insertAdjacentHTML('afterbegin', `<img src="${articleData[item].imageUrl}" alt="${articleData[item].altTxt}"></img>`)
-// document.getElementById('title')
-//     .insertAdjacentHTML('afterbegin', `${articleData[item].name}`)
-// document.getElementById('price')
-//     .insertAdjacentHTML('afterbegin', `${articleData[item].price}`)
-// document.getElementById('description')
-//     .insertAdjacentHTML('afterbegin', `${articleData[item].description}`)
-
-
-
-// function saveBasket(item) {
-//     localStorage.setItem("basket", JSON.stringify(item));
-// }
-
-// function getNewPurchase() {
-//     let basket = localStorage.getItem("basket");
-//     if (basket == null) {
-//         return [];
-//     } else {
-//         return JSON.parse(basket);
-//     }
-// }
-
-
-// }
